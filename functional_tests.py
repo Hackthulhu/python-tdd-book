5fd32cb0c515f59ce30560fd9fecd9ecb7a7e2cf
Galen Evans <gaevans@ebay.com>
2020-09-04 11:19:49 -0700

    Functional tests now checks we can inoput a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -14,18 +14,35 @@ class NewVisitorTest(unittest.TestCase):
 
         #he notives the page titl and header mention to-do lists
         self.assertIn('To-Do',  self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').header_text
+        self.assertIn('To-Do', header_text)
+
 
         #He is invited to enter a to-do list item staright away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
 
         #he types "Align the conjuntion of the spheres" into a text-box
+        inputbox.sendKeys('Align the conjunction of the spheres')
+
 
         # When he hits enter the page updates and now has 1: Align the conjuntion of
         # spheres" as an item in the to-do lists
+        inputbox.sendKeys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == "1: Align the conjuntion of the spheres" for row in rows)
+        )
 
         # There is still a text box inviting him to add another item he enters
         # "Prepare the Ochre for the ritual of invocation"
-
+        self.fail("Finish the Test")
         # The page updates again and the first two tasks appear on his list.
 
         # Worried about this arcane formula being destroyed into the ether, Fizzywig
