cc87703ae9d0cca5ee325efbe141e4986b2a2573
Galen Evans <gaevans@ebay.com>
2020-09-08 07:58:55 -0700

    changed model to allow for 'Todos'

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    pass
+
 class Item(models.Model):
     text = models.TextField(default='')
+    todo = models.ForeignKey(Todo, default=None)
--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, Todo
 
 from lists.views import home_page
 
@@ -35,8 +35,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey1')
-        Item.objects.create(text='itemey2')
+        todo = Todo.objects.create()
+        Item.objects.create(text='itemey1', todo=todo)
+        Item.objects.create(text='itemey2', todo=todo)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -44,15 +45,23 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey2')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retreiving_items(self):
+        todo = Todo()
+        todo.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.todo = todo
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.todo = todo
         second_item.save()
+
+        saved_todo = Todo.objects.first()
+        self.assertEqual(saved_todo, todo)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -60,4 +69,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.todo, todo)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.todo, todo)
